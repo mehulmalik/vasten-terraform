94c3411a569bad3bd97157d5642c2a5cd1c10b68
mehulmalik <54393241+mehulmalik@users.noreply.github.com>
2020-04-11 04:56:23 +0530

    Update vars.tf

--- modules/cluster/vars.tf
+++ modules/cluster/vars.tf
@@ -18,6 +18,11 @@ variable "name" {
   type        = string
 }
 
+variable "localStore_capacity" {
+  
+}
+
+
 variable "network" {
   description = "A reference (self link) to the VPC network to host the cluster in"
   type        = string
@@ -209,5 +214,5 @@ variable "ip_addrs" {
 
 
 variable "initial_node_count" {
-  
+
 }
